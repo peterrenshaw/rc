d0664821b24cde90974d3472a0cef98aa0329a37
Peter Renshaw <goonmail@netspace.net.au>
2013-08-21 13:06:49 +1000

    2013AUG21 * added hello.sh - bash script gretter shows various   stuff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+2013AUG21
+* hello.sh
+- greeter when you log into bash
+- shows calander, who's logged in, datetime in my format & logins
+
 2013JUL26
 * .vimrc
 * hello world
---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#=== 
+# name: hello.sh
+# programmer: pr
+# date: 2013APR13
+# description: 
+#     user infor for login, current date & time
+#===
+clear
+cal
+echo "Hey \033[1m$USER\033[0m it's \033[1m`date '+%A %d %B @ %T'`\033[0m" | tr '[A-Z]' '[a-z]' 
+echo "logins:\c" ; who | wc -l
+#exit 0
