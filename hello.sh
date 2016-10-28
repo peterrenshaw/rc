e23cc695e9392245a5959bfeb099b54f57c5bf8d
Peter Renshaw <peterrenshaw@seldomlogical.com>
2016-10-28 14:04:33 +1100

    2016OCT28 updated hello.sh cf https://news.ycombinator.com/item?id=11682202

--- hello.sh
+++ hello.sh
@@ -2,13 +2,23 @@
 
 #=== 
 # name: hello.sh
-# programmer: pr
+# prog: pr
 # date: 2013APR13
-# description: 
-#     user infor for login, current date & time
+# desc:
+#      latest status for user
+# updt: 
+# * 2016OCT28
+# - fixed cf this HN comment
+#   <https://news.ycombinator.com/item?id=11682202>
+# * 2016OCT08 
+#   updated for mac
+#   user infor for login, current date & time
 #===
-clear
-cal
-echo "Hey \033[1m$USER\033[0m it's \033[1m`date '+%A %d %B @ %T'`\033[0m" | tr '[A-Z]' '[a-z]' 
-echo "logins:\c" ; who | wc -l
-#exit 0
+{
+    clear
+    cal
+    echo "Hey \033[1m$USER\033[0m it's \033[1m`date '+%A %d %B @ %T'`\033[0m" | tr '[A-Z]' '[a-z]' 
+    echo "logins:\c" ; who | wc -l
+    cat $HOME/TODO
+    #exit 0
+} >&2
